1a27d05f1e1b62e429b1d6a3d95a46310e749c22
Ken Wakita <wakita@is.titech.ac.jp>
2015-11-18 17:33:51 +0900

    Removed unnecessary `m_context->setFormat(fmt)`

--- main.cxx
+++ main.cxx
@@ -9,23 +9,18 @@ extern Ui::MainWindow mainWindow;
 
 int main(int argc, char *argv[])
 {
-  std::cerr << "starting" << std::endl;
-
   QApplication a(argc, argv);
 
   QSurfaceFormat fmt;
-  fmt.setMajorVersion(4);
-  fmt.setMinorVersion(3);
+  fmt.setMajorVersion(4); fmt.setMinorVersion(5);
   fmt.setProfile(QSurfaceFormat::CoreProfile);
   fmt.setSamples(4);
   fmt.setDepthBufferSize(24);
-
   QSurfaceFormat::setDefaultFormat(fmt);
 
   MainWindow *w = new MainWindow;
 
   QOpenGLContext *m_context = new QOpenGLContext;
-  m_context->setFormat(fmt);
   m_context->create();
 
   std::map<QSurfaceFormat::OpenGLContextProfile, std::string> profile_str = {
@@ -33,9 +28,7 @@ int main(int argc, char *argv[])
     { QSurfaceFormat::OpenGLContextProfile::CoreProfile, "Core Profile"},
     { QSurfaceFormat::OpenGLContextProfile::CompatibilityProfile, "Compatibility Profile"}
   };
-
   fmt = m_context->format();
-
   qWarning("OpenGL %d.%d %s", fmt.version().first, fmt.version().second, profile_str[fmt.profile()].c_str());
 
   w->show();
